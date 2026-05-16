97aee95c01f95ece8b6c453994c428f99ac3d716
PedroBentoDev <pedroherinquebentomartins@gmail.com>
2026-05-16 19:14:04 -0300

    Adiciona método DELETE e ajustes na API

--- src/index.js
+++ src/index.js
@@ -1,38 +1,67 @@
- const express = require('express');
- const app = express (); /* criasnado uma instancia do express*/
- const port = 3000; /* definindo a porta do servidor */
+const express = require('express');
+const app = express(); /* criando uma instância do express */
+const port = 3000;
 
-app.use(express.json()):
-const mongooose = require('mongooose');
+app.use(express.json());
 
+const mongoose = require('mongoose');
 
-/*objeto que será enviado para banco de bd*/
+/* Modelo do banco */
 const Film = mongoose.model('Film', {
-      title: String,
-      description : String,
-      image_url : String,
-      trailer_url : String,
+    title: String,
+    description: String,
+    image_url: String,
+    trailer_url: String
 });
 
-app.get('/', (req, res) => { /* definindo a rota raiz do servidor */
- const films  = await Film.find()
- res.send(films); /* enviando uma resposta para o cliente */
+
+/* Buscar filmes */
+app.get('/', async (req, res) => {
+
+    const films = await Film.find();
+
+    res.send(films);
+
 });
 
-app.post("/", async(req,res ) => { 
-  const film = new film({ 
-   tittle : req.body.tittle,
-   description : req.body.description,
-   image_url: req.body.image_url,
-   trailer_url : req.body.trailer_url
-  })
- 
- await  film.save();
- res.send(film)
-})
+
+/* Criar filme */
+app.post('/', async (req, res) => {
+
+    const film = new Film({
+
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer_url: req.body.trailer_url
+
+    });
+
+    await film.save();
+
+    res.send(film);
+
+});
 
 
-app.listen (port,() => {
- mongooose.connect('');
- console.log("app running");
- });
+app.delete('/films/:id', async (req, res) => {
+
+    const id = req.params.id;
+
+    const film = await Film.findByIdAndDelete(id);
+
+    res.send(film);
+
+});
+
+/* Iniciar servidor */
+app.listen(port, async () => {
+
+    await mongoose.connect(
+    ''
+);
+    console.log("Conectado ao banco de dados");
+    console.log("App running");
+
+});
+
